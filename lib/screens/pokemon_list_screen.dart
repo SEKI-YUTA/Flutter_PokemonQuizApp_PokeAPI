7f1300e5eaf680016dda7bfad976ee92b4c8aa20
SEKI-YUTA <zard.since1991@gmail.com>
2024-04-09 02:13:54 +0900

    ポケモンリストを無限スクロールできるように変更

--- lib/screens/pokemon_list_screen.dart
+++ lib/screens/pokemon_list_screen.dart
@@ -1,4 +1,5 @@
 import 'dart:convert';
+import 'dart:ffi';
 
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
@@ -15,25 +16,55 @@ class _PokemonListScreenState extends State<PokemonListScreen> {
   final String _pokemonListEndPoint = 'pokemon';
   List pokemonList = [];
   String? nextURL;
+  bool _isLoading = false;
+  late ScrollController scrollController;
 
-  Future<void> _fetchPokemonList() async {
-    var response = await http.get(Uri.parse(_baseURL + _pokemonListEndPoint));
+  Future<void> _fetchPokemonList(bool? appendMode) async {
+    appendMode ??= false;
+    setState(() {
+      _isLoading = true;
+    });
+    final String fetchURL =
+        nextURL != null ? nextURL! : _baseURL + _pokemonListEndPoint;
+    var response = await http.get(Uri.parse(fetchURL));
     var decoded = jsonDecode(response.body);
     setState(() {
       nextURL = decoded['next'];
-      pokemonList = decoded['results'];
+      pokemonList = appendMode!
+          ? [...pokemonList, ...decoded['results']]
+          : decoded['results'];
+      _isLoading = false;
     });
   }
 
   @override
   void initState() {
+    _fetchPokemonList(null);
+    scrollController = ScrollController();
+    scrollController.addListener(() async {
+      print("max: ${scrollController.position.maxScrollExtent}");
+      print("pos: ${scrollController.position.pixels}");
+      if (scrollController.position.pixels >=
+              scrollController.position.maxScrollExtent * 0.95 &&
+          !_isLoading) {
+        if (nextURL != null) {
+          setState(() {
+            _isLoading = true;
+          });
+          _fetchPokemonList(true);
+          setState(() {
+            _isLoading = false;
+          });
+        }
+      }
+    });
     super.initState();
-    _fetchPokemonList();
   }
 
   @override
   Widget build(BuildContext context) {
     return ListView.builder(
+        controller: scrollController,
         itemCount: pokemonList.length,
         itemBuilder: (context, index) {
           var pokemon = pokemonList[index];
