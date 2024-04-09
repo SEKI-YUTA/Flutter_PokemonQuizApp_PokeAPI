4030f280734fd2acc3e01ed4421cd1278cec41dd
SEKI-YUTA <zard.since1991@gmail.com>
2024-04-09 11:49:28 +0900

    ポケモンの情報を表示するコンポーネントを追加

---
+++ lib/components/PokemonCard.dart
@@ -0,0 +1,70 @@
+import 'dart:convert';
+
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'package:pokemon_quiz_app/model/PokemonListItem.dart';
+
+class PokemonCard extends StatefulWidget {
+  PokemonListItem item;
+  PokemonCard({super.key, required this.item});
+
+  @override
+  State<PokemonCard> createState() => _PokemonCardState();
+}
+
+class _PokemonCardState extends State<PokemonCard> {
+  String _imageUrlStr = "";
+  bool _isLoading = true;
+
+  Future<void> _fetchPokemonInfo() async {
+    print(widget.item.url);
+    var response = await http.get(Uri.parse(widget.item.url));
+    var decoded = jsonDecode(response.body);
+    print("card: $decoded");
+    print(decoded['sprites']['other']['official-artwork']['front_default']);
+    if (mounted) {
+      setState(() {
+        _imageUrlStr =
+            decoded['sprites']['other']['official-artwork']['front_default'];
+        _isLoading = false;
+      });
+    }
+  }
+
+  @override
+  initState() {
+    super.initState();
+    _fetchPokemonInfo();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: _isLoading
+          ? const LoadingContent()
+          : Row(
+              children: [
+                Image.network(
+                  _imageUrlStr,
+                  width: 100,
+                  height: 100,
+                ),
+                Text(widget.item.name,
+                    style: Theme.of(context).textTheme.headlineMedium),
+              ],
+            ),
+    );
+  }
+}
+
+class LoadingContent extends StatelessWidget {
+  const LoadingContent({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Center(
+      child: CircularProgressIndicator(),
+    );
+  }
+}
