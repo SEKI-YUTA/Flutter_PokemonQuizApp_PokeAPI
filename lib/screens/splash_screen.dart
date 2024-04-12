f1054cbd9f108c1ce32a3e5daad37b18056cb858
SEKI-YUTA <zard.since1991@gmail.com>
2024-04-13 00:15:08 +0900

    スプラッシュ画面と認証画面のファイルを作成

---
+++ lib/screens/splash_screen.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({super.key});
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: Column(children: [
+      const Text("Splash Screen"),
+      ElevatedButton(
+          onPressed: () {
+            Navigator.of(context).pushNamed('/auth');
+          },
+          child: const Text("navigate to auth screen"))
+    ]));
+  }
+}
