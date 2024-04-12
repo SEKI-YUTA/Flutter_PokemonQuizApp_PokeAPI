02281cda4c71cd94e9954a31b3df490ef20747b8
SEKI-YUTA <zard.since1991@gmail.com>
2024-04-13 00:15:08 +0900

    スプラッシュ画面と認証画面のファイルを作成

---
+++ lib/screens/auth_screen.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+import 'package:sign_in_button/sign_in_button.dart';
+
+class AuthScreen extends StatefulWidget {
+  const AuthScreen({super.key});
+
+  @override
+  State<AuthScreen> createState() => _AuthScreenState();
+}
+
+class _AuthScreenState extends State<AuthScreen> {
+  bool _isLoginMode = false;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: const Text('Login'),
+        ),
+        body: Column(
+          children: [
+            Text(_isLoginMode ? "ログイン" : "新規登録"),
+            SignInButton(Buttons.google, onPressed: () {}),
+            ElevatedButton(
+                onPressed: () {
+                  Navigator.of(context).pushNamed('/mainHost');
+                },
+                child: Text("Navigate to mainHost"))
+          ],
+        ));
+  }
+}
