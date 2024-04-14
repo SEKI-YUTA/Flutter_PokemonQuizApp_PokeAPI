9ffe14a4876c5fa10685cb514da50732877c531d
SEKI-YUTA <zard.since1991@gmail.com>
2024-04-14 18:07:16 +0900

    create component of will use in quiz screen

---
+++ lib/components/HintItems.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+import 'package:pokemon_quiz_app/components/status_item.dart';
+import 'package:pokemon_quiz_app/data/model/PokemonData.dart';
+
+class HintItems {
+  static ExpansionTile hint1ExpansionTile(
+      BuildContext context, PokemonData pokemonData) {
+    return ExpansionTile(
+        title: const Text("ヒント1"),
+        initiallyExpanded: true,
+        children: [
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                "基本ステータス",
+                style: Theme.of(context).textTheme.bodyLarge,
+              ),
+              const SizedBox(
+                height: 8,
+              ),
+              StatusItem(title: "こうげき", value: pokemonData.attack),
+              StatusItem(title: "ぼうぎょ", value: pokemonData.defense),
+              StatusItem(title: "とくこう", value: pokemonData.specialAttack),
+              StatusItem(title: "とくぼう", value: pokemonData.specialDefense),
+              StatusItem(title: "すばやさ", value: pokemonData.speed),
+            ],
+          ),
+        ]);
+  }
+}
