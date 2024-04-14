07f0ff219a560e10d2376accf759e34dd43cc5ac
SEKI-YUTA <zard.since1991@gmail.com>
2024-04-14 18:07:29 +0900

    create raf ui of quiz screen

--- lib/screens/pokemon_quiz_screen.dart
+++ lib/screens/pokemon_quiz_screen.dart
@@ -1,8 +1,12 @@
 import 'dart:convert';
 
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 import 'package:http/http.dart' as http;
+import 'package:pokemon_quiz_app/components/HintItems.dart';
 import 'package:pokemon_quiz_app/components/center_message.dart';
+import 'package:pokemon_quiz_app/components/status_item.dart';
 import 'package:pokemon_quiz_app/data/PokeApi.dart';
 import 'package:pokemon_quiz_app/data/model/PokemonData.dart';
 import 'dart:math' as math;
@@ -17,14 +21,19 @@ class PokemonQuizScreen extends StatefulWidget {
 
 class _PokemonQuizScreenState extends State<PokemonQuizScreen> {
   PokemonData? pokemonData;
+  bool _isLoading = false;
   final TextEditingController _userAnswerController = TextEditingController();
   Future<void> _fetchRandomPokemonData() async {
+    setState(() {
+      _isLoading = true;
+    });
     var maxCount = 1025;
     var randomNumber = math.Random().nextInt(maxCount) + 1;
     var data = await PokeApi.fetchPokemonDetail(
         PokeApiEndpoints.createPokemonDetailURL(randomNumber.toString()));
     setState(() {
       pokemonData = data;
+      _isLoading = false;
     });
   }
 
@@ -37,39 +46,61 @@ class _PokemonQuizScreenState extends State<PokemonQuizScreen> {
 
   @override
   Widget build(BuildContext context) {
-    return pokemonData != null
-        ? Column(
-            children: [
-              Image.network(pokemonData!.pokemonImageURL),
-              Text(
-                pokemonData!.pokemonName,
-                style: Theme.of(context).textTheme.headlineSmall,
-              ),
-              Text("基本ステータス"),
-              Column(
+    return !_isLoading && pokemonData != null
+        ? Padding(
+            padding: const EdgeInsets.all(16),
+            child: SingleChildScrollView(
+              child: Column(
                 children: [
-                  Text("こうげき"),
-                  Text("ぼうぎょ"),
-                  Text("とくこう"),
-                  Text("とくぼう"),
-                  Text("すばやさ"),
-                  Text("HP")
+                  SizedBox(
+                    width: double.infinity,
+                    child: ConstrainedBox(
+                      constraints:
+                          const BoxConstraints(maxWidth: 300, maxHeight: 300),
+                      child: SizedBox(
+                        width: double.infinity,
+                        height: double.infinity,
+                        child: Image.network(pokemonData!.pokemonImageURL),
+                      ),
+                    ),
+                  ),
+                  Text(pokemonData!.pokemonName),
+                  Text(
+                    "このポケモンの名前は？",
+                    style: Theme.of(context)
+                        .textTheme
+                        .headlineSmall!
+                        .copyWith(color: Colors.red),
+                  ),
+                  const SizedBox(
+                    height: 16,
+                  ),
+                  HintItems.hint1ExpansionTile(context, pokemonData!),
+                  const SizedBox(height: 16),
+                  TextField(
+                    controller: _userAnswerController,
+                    decoration: const InputDecoration(
+                      hintText: "ポケモンの名前(例：ピカチュウ)",
+                    ),
+                    maxLines: 1,
+                  ),
+                  const SizedBox(
+                    height: 8,
+                  ),
+                  ElevatedButton(
+                      onPressed: () {}, child: const Text("こたえ合わせをする")),
+                  const SizedBox(height: 40),
+                  ElevatedButton(onPressed: () {}, child: const Text("答えを見る")),
+                  Row(mainAxisAlignment: MainAxisAlignment.end, children: [
+                    ElevatedButton(
+                        onPressed: () {
+                          _fetchRandomPokemonData();
+                        },
+                        child: const Text("次の問題")),
+                  ])
                 ],
               ),
-              TextField(
-                controller: _userAnswerController,
-                decoration: const InputDecoration(
-                  hintText: "ポケモンの名前(例：ピカチュウ)",
-                ),
-                maxLines: 1,
-              ),
-              ElevatedButton(onPressed: () {}, child: const Text("答えを見る")),
-              ElevatedButton(
-                  onPressed: () {
-                    _fetchRandomPokemonData();
-                  },
-                  child: const Text("次の問題")),
-            ],
+            ),
           )
         : const CenterMessage(
             message: "読み込み中...",
