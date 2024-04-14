de5ddb0d9bd93ec1a035212ea65fb3a3fe0a79f6
SEKI-YUTA <zard.since1991@gmail.com>
2024-04-14 15:42:58 +0900

    extract program of call PokeApi

---
+++ lib/data/PokeApi.dart
@@ -0,0 +1,42 @@
+import 'dart:convert';
+
+import 'package:pokemon_quiz_app/model/PokemonListItem.dart';
+import 'package:http/http.dart' as http;
+import 'package:pokemon_quiz_app/other/PokeApiEndpoints.dart';
+
+class PokeApi {
+  static Future<(List<PokemonListItem?>, String)> fetchPokemonList(String? fetchUrl) async {
+    fetchUrl ??= "${PokeApiEndpoints.BASE_URL}/${PokeApiEndpoints.POKEMON}";
+    var response = await http.get(Uri.parse(fetchUrl));
+    var decoded = jsonDecode(response.body);
+    String nextUrl = decoded['next'];
+    List<Future<void>> futureList = [];
+    List<PokemonListItem?> list =
+        List.generate(decoded['results'].length, (item) => null);
+    for (int i = 0; i < decoded['results'].length; i++) {
+      var item = decoded['results'][i];
+      var f = fetchPokemonDetail(item['url']).then((value) {
+        list[i] = value;
+      });
+      futureList.add(f);
+    }
+    await Future.wait(futureList);
+    return (list, nextUrl);
+  }
+  
+  static Future<PokemonListItem> fetchPokemonDetail(String detailURL) async {
+    var detailResponse = await http.get(Uri.parse(detailURL));
+    var detailDecoded = jsonDecode(detailResponse.body);
+    var speciesResponse =
+        await http.get(Uri.parse(detailDecoded['species']['url']));
+    var speciesDecoded = jsonDecode(speciesResponse.body);
+    var data = PokemonListItem(
+      pokemonName: speciesDecoded['names']
+          .where((item) => item['language']['name'] == 'ja-Hrkt')
+          .first['name'],
+      pokemonImageURL: detailDecoded['sprites']['other']['official-artwork']
+          ['front_default'],
+    );
+    return data;
+  }
+}
--- lib/screens/pokemon_quiz_screen.dart
+++ lib/screens/pokemon_quiz_screen.dart
@@ -1,12 +1,80 @@
-import 'package:flutter/material.dart';
+import 'dart:convert';
 
-class PokemonQuizScreen extends StatelessWidget {
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'dart:math' as math;
+import 'package:pokemon_quiz_app/other/PokeApiEndpoints.dart';
+
+class PokemonQuizScreen extends StatefulWidget {
   const PokemonQuizScreen({super.key});
 
   @override
+  State<PokemonQuizScreen> createState() => _PokemonQuizScreenState();
+}
+
+class _PokemonQuizScreenState extends State<PokemonQuizScreen> {
+  String? imageURL;
+  String? pokemonName;
+  final TextEditingController _userAnswerController = TextEditingController();
+  Future<void> _fetchRandomPokemonData() async {
+    var maxCount = 1025;
+    var randomNumber = math.Random().nextInt(maxCount) + 1;
+    var pokemonData = await http.get(Uri.parse(
+        PokeApiEndpoints.createPokemonDetailURL(randomNumber.toString())));
+    var parsedData = jsonDecode(pokemonData.body);
+    print("randomId: $randomNumber");
+    print(parsedData['name']);
+    print(parsedData['sprites']['other']['front_default']);
+    setState(() {
+      pokemonName = parsedData['name'];
+      imageURL =
+          parsedData['sprites']['other']['official-artwork']['front_default'];
+    });
+  }
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    _fetchRandomPokemonData();
+    super.initState();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: Text("クイズ画面"),
+    return Column(
+      children: [
+        imageURL != null ? Image.network(imageURL!) : Container(),
+        pokemonName != null
+            ? Text(
+                pokemonName!,
+                style: Theme.of(context).textTheme.headlineSmall,
+              )
+            : Container(),
+        Text("基本ステータス"),
+        Column(
+          children: [
+            Text("こうげき"),
+            Text("ぼうぎょ"),
+            Text("とくこう"),
+            Text("とくぼう"),
+            Text("すばやさ"),
+            Text("HP")
+          ],
+        ),
+        TextField(
+          controller: _userAnswerController,
+          decoration: const InputDecoration(
+            hintText: "ポケモンの名前(例：ピカチュウ)",
+          ),
+          maxLines: 1,
+        ),
+        ElevatedButton(onPressed: () {}, child: const Text("答えを見る")),
+        ElevatedButton(
+            onPressed: () {
+              _fetchRandomPokemonData();
+            },
+            child: const Text("次の問題")),
+      ],
     );
   }
 }
