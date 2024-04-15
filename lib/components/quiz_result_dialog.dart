b3455ab72baf1185d246a04fe9d6a09e87660d7f
SEKI-YUTA <zard.since1991@gmail.com>
2024-04-15 11:13:36 +0900

    add showing pokemonName and pokemonImage in quiz_result_dialog.dart

--- lib/components/quiz_result_dialog.dart
+++ lib/components/quiz_result_dialog.dart
@@ -2,9 +2,15 @@ import 'package:flutter/material.dart';
 
 class QuizResultDialog extends StatelessWidget {
   final bool isCorrect;
+  final String pokemonName;
+  final String pokemonImageUrl;
   final Function() onNextClick;
   const QuizResultDialog(
-      {super.key, required this.isCorrect, required this.onNextClick});
+      {super.key,
+      required this.isCorrect,
+      required this.pokemonName,
+      required this.pokemonImageUrl,
+      required this.onNextClick});
 
   @override
   Widget build(BuildContext context) {
@@ -16,6 +22,9 @@ class QuizResultDialog extends StatelessWidget {
           children: [
             const Text("クイズの結果"),
             Text(isCorrect ? "正解" : "不正解"),
+            Image.network(width: 200, height: 200, pokemonImageUrl),
+            Text(pokemonName),
+            const SizedBox(height: 16),
             ElevatedButton(onPressed: onNextClick, child: const Text("次の問題へ"))
           ],
         ),
