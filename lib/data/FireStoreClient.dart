951686f33b8e8781c21c05725e5da210e7d453a4
SEKI-YUTA <zard.since1991@gmail.com>
2024-04-15 17:54:52 +0900

    add method of read caughtPokemon

--- lib/data/FireStoreClient.dart
+++ lib/data/FireStoreClient.dart
@@ -1,6 +1,7 @@
 import 'dart:ffi';
 
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:pokemon_quiz_app/data/model/CaughtPokemon.dart';
 
 class FireStoreClient {
   static addCaughtPokemon(
@@ -15,7 +16,20 @@ class FireStoreClient {
         .doc(pokemonId.toString())
         .set({
       "pokemonId": pokemonId,
-      "caughtAtSec": caughtAt,
+      "caughtAt": caughtAt,
     });
+  }
+
+  static Future<List<CaughtPokemon>> getCaughtPokemonList() async {
+    final snapshot = await FirebaseFirestore.instance
+        .collection("users")
+        .doc("testUser1")
+        .collection("caughtPokemonList")
+        .get();
+    snapshot.docs.map((doc) => doc.data()["pokemonId"]).toList();
+    return snapshot.docs
+        .map((pokemon) => CaughtPokemon(
+            id: pokemon['pokemonId'], caughtAt: pokemon['caughtAt']))
+        .toList();
   }
 }
--- lib/screens/basket_screen.dart
+++ lib/screens/basket_screen.dart
@@ -1,7 +1,25 @@
 import 'package:flutter/material.dart';
+import 'package:pokemon_quiz_app/data/FireStoreClient.dart';
 
-class BasketScreen extends StatelessWidget {
+class BasketScreen extends StatefulWidget {
   const BasketScreen({super.key});
+
+  @override
+  State<BasketScreen> createState() => _BasketScreenState();
+}
+
+class _BasketScreenState extends State<BasketScreen> {
+  Future<void> _readData() async {
+    var list = await FireStoreClient.getCaughtPokemonList();
+    print("caughtPokemonList $list");
+  }
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    _readData();
+  }
 
   @override
   Widget build(BuildContext context) {
