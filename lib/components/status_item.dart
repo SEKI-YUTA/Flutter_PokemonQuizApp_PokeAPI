06e0a0480c0e377ec69faa5920e7b1565c7332df
SEKI-YUTA <zard.since1991@gmail.com>
2024-04-16 02:11:40 +0900

    add requiredIndeicator arguments

--- lib/components/status_item.dart
+++ lib/components/status_item.dart
@@ -5,34 +5,42 @@ class StatusItem extends StatelessWidget {
   final String title;
   final int value;
   final statusMax = 255;
+  final bool requireIndicator;
   final BuildContext parentContext;
   const StatusItem(
       {super.key,
       required this.title,
       required this.value,
+      this.requireIndicator = true,
       required this.parentContext});
 
   @override
   Widget build(BuildContext context) {
     final myStyle = Theme.of(parentContext).textTheme.bodyLarge;
-    return Row(
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: [
-        Text(
-          title,
-          style: myStyle,
-        ),
-        const SizedBox(width: 16),
-        Text(value.toString()),
-        const SizedBox(width: 8),
-        Expanded(
-          child: LinearProgressIndicator(
-            value: value / statusMax,
-            backgroundColor: Colors.grey,
-            valueColor: const AlwaysStoppedAnimation<Color>(Colors.green),
+    return Padding(
+      padding: const EdgeInsets.only(bottom: 8),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.start,
+        children: [
+          Text(
+            title,
+            style: myStyle,
           ),
-        ),
-      ],
+          const SizedBox(width: 16),
+          Text(value.toString()),
+          const SizedBox(width: 8),
+          requireIndicator
+              ? Expanded(
+                  child: LinearProgressIndicator(
+                    value: value / statusMax,
+                    backgroundColor: Colors.grey,
+                    valueColor:
+                        const AlwaysStoppedAnimation<Color>(Colors.green),
+                  ),
+                )
+              : Container(),
+        ],
+      ),
     );
   }
 }
