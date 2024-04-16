4d6d0798acaed28e93f3944a8eea7d3c71730d84
SEKI-YUTA <zard.since1991@gmail.com>
2024-04-16 13:59:14 +0900

    add showing caught pokemon list

--- lib/screens/box_screen.dart
+++ lib/screens/box_screen.dart
@@ -1,5 +1,13 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import 'package:pokemon_quiz_app/components/center_message.dart';
+import 'package:pokemon_quiz_app/components/pokemon_card.dart';
 import 'package:pokemon_quiz_app/data/FireStoreClient.dart';
+import 'package:pokemon_quiz_app/data/PokeApi.dart';
+import 'package:pokemon_quiz_app/data/model/CaughtPokemon.dart';
+import 'package:pokemon_quiz_app/data/model/PokemonData.dart';
+import 'package:pokemon_quiz_app/other/PokeApiEndpoints.dart';
 
 class BoxScreen extends StatefulWidget {
   const BoxScreen({super.key});
@@ -9,22 +17,92 @@ class BoxScreen extends StatefulWidget {
 }
 
 class _BoxScreenState extends State<BoxScreen> {
-  Future<void> _readData() async {
-    var list = await FireStoreClient.getCaughtPokemonList();
-    print("caughtPokemonList $list");
+  final List<PokemonData> _pokemonList = [];
+  int _fetchedIndex = 0;
+  final int _FETCH_COUNT = 10;
+  bool _isLoading = false;
+  bool _isFullLoaded = false;
+  late ScrollController scrollController;
+
+  Future<void> _getCaughtPokemonData() async {
+    setState(() {
+      _isLoading = true;
+    });
+    var caughtPokemonList = await FireStoreClient.getCaughtPokemonList();
+    var addPokemonList = <PokemonData>[];
+    var futureList = <Future>[];
+    for (int i = _fetchedIndex; i < (_fetchedIndex + _FETCH_COUNT); i++) {
+      if (i >= caughtPokemonList.length) {
+        setState(() {
+          _isFullLoaded = true;
+        });
+        break;
+      }
+      var f = PokeApi.fetchPokemonDetail(
+              PokeApiEndpoints.createPokemonDetailURL(
+                  caughtPokemonList[i].id.toString()))
+          .then((pokemon) => addPokemonList.add(pokemon));
+      futureList.add(f);
+    }
+    await Future.wait(futureList);
+    if (mounted) {
+      setState(() {
+        _fetchedIndex += _FETCH_COUNT;
+        _pokemonList.addAll(addPokemonList);
+        _isLoading = false;
+      });
+    }
   }
 
   @override
   void initState() {
-    // TODO: implement initState
+    scrollController = ScrollController();
+    scrollController.addListener(() async {
+      if (scrollController.position.pixels >=
+              scrollController.position.maxScrollExtent * 0.95 &&
+          !_isLoading) {
+        if (_isFullLoaded == false) {
+          _getCaughtPokemonData();
+        }
+      }
+    });
+    _getCaughtPokemonData();
     super.initState();
-    _readData();
   }
 
   @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: Text("BOX画面"),
-    );
+    return SizedBox(
+        width: MediaQuery.of(context).size.width,
+        height: MediaQuery.of(context).size.height,
+        child: _isLoading && _pokemonList.isEmpty
+            ? const CenterMessage(
+                message: "読み込み中...",
+                showingLoadingIndicatoro: true,
+              )
+            : Column(
+                children: [
+                  Text(
+                    "捕まえたポケモン",
+                    style: Theme.of(context).textTheme.headlineMedium,
+                  ),
+                  Flexible(
+                    child: ListView.builder(
+                        itemCount: _pokemonList.length,
+                        itemBuilder: (context, index) {
+                          var pokemonData = _pokemonList[index];
+                          return PokemonCard(item: pokemonData);
+                        }),
+                  ),
+                ],
+              ));
+    // return Container(
+    //   child: ElevatedButton(
+    //     child: Text("hogheoge"),
+    //     onPressed: () {
+    //       print(_pokemonList);
+    //     },
+    //   ),
+    // );
   }
 }
