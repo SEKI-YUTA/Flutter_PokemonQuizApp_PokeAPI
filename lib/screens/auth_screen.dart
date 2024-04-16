19ca1996dded9363ddb14e105dd0ef943ac18ac0
SEKI-YUTA <zard.since1991@gmail.com>
2024-04-16 14:58:30 +0900

    wrap screen with SafeArea widget

--- lib/screens/auth_screen.dart
+++ lib/screens/auth_screen.dart
@@ -18,87 +18,89 @@ class _AuthScreenState extends State<AuthScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: SizedBox(
-      width: double.infinity,
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          ConstrainedBox(
-            constraints: const BoxConstraints.tightFor(width: 300),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Text(
-                  _isLoginMode ? "ログイン" : "新規登録",
-                  style: Theme.of(context).textTheme.headlineSmall,
-                ),
-                TextField(
-                  controller: _emailController,
-                  decoration: const InputDecoration(label: Text("メールアドレス")),
-                ),
-                TextField(
-                  controller: _passwordController,
-                  obscureText: !_isPasswordVisible,
-                  decoration: InputDecoration(
-                      label: const Text("パスワード"),
-                      suffixIcon: IconButton(
-                        icon: Icon(_isPasswordVisible
-                            ? Icons.visibility
-                            : Icons.visibility_off),
-                        onPressed: () {
-                          setState(() {
-                            _isPasswordVisible = !_isPasswordVisible;
-                          });
-                        },
-                      )),
-                ),
-                const SizedBox(height: 8),
-                ElevatedButton(
-                    onPressed: () {
-                      print("email: ${_emailController.text}");
-                      print("password: ${_passwordController.text}");
-                    },
-                    child: Text(_isLoginMode ? "ログイン" : "新規登録")),
-                const SizedBox(height: 8),
-                const Divider(),
-                const SizedBox(height: 8),
-                SignInButton(Buttons.google, onPressed: () {
-                  FirebaseAuth.instance
-                      .signInWithProvider(GoogleAuthProvider())
-                      .then((result) {
-                    print("auth result: $result");
-                    if (result.user != null) {
-                      Navigator.of(context).pushReplacementNamed('/mainHost');
-                      FirebaseFirestore.instance
-                          .collection("users")
-                          .doc(result.user!.uid)
-                          .set({
-                        "created_at": DateTime.now().millisecondsSinceEpoch,
-                        "name": result.user!.displayName,
-                        "email": result.user!.email,
-                        "photoURL": result.user!.photoURL
-                      });
-                    }
-                  });
-                }),
-                TextButton(
-                    onPressed: () {
-                      setState(() {
-                        _isLoginMode = !_isLoginMode;
-                      });
-                      _emailController.text = "";
-                      _passwordController.text = "";
-                    },
-                    child: Text(_isLoginMode ? "新規登録画面へ" : "ログイン画面へ")),
-                ElevatedButton(
-                    onPressed: () {
-                      Navigator.of(context).pushReplacementNamed('/mainHost');
-                    },
-                    child: Text("Navigate to mainHost"))
-              ],
+        body: SafeArea(
+      child: SizedBox(
+        width: double.infinity,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            ConstrainedBox(
+              constraints: const BoxConstraints.tightFor(width: 300),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text(
+                    _isLoginMode ? "ログイン" : "新規登録",
+                    style: Theme.of(context).textTheme.headlineSmall,
+                  ),
+                  TextField(
+                    controller: _emailController,
+                    decoration: const InputDecoration(label: Text("メールアドレス")),
+                  ),
+                  TextField(
+                    controller: _passwordController,
+                    obscureText: !_isPasswordVisible,
+                    decoration: InputDecoration(
+                        label: const Text("パスワード"),
+                        suffixIcon: IconButton(
+                          icon: Icon(_isPasswordVisible
+                              ? Icons.visibility
+                              : Icons.visibility_off),
+                          onPressed: () {
+                            setState(() {
+                              _isPasswordVisible = !_isPasswordVisible;
+                            });
+                          },
+                        )),
+                  ),
+                  const SizedBox(height: 8),
+                  ElevatedButton(
+                      onPressed: () {
+                        print("email: ${_emailController.text}");
+                        print("password: ${_passwordController.text}");
+                      },
+                      child: Text(_isLoginMode ? "ログイン" : "新規登録")),
+                  const SizedBox(height: 8),
+                  const Divider(),
+                  const SizedBox(height: 8),
+                  SignInButton(Buttons.google, onPressed: () {
+                    FirebaseAuth.instance
+                        .signInWithProvider(GoogleAuthProvider())
+                        .then((result) {
+                      print("auth result: $result");
+                      if (result.user != null) {
+                        Navigator.of(context).pushReplacementNamed('/mainHost');
+                        FirebaseFirestore.instance
+                            .collection("users")
+                            .doc(result.user!.uid)
+                            .set({
+                          "created_at": DateTime.now().millisecondsSinceEpoch,
+                          "name": result.user!.displayName,
+                          "email": result.user!.email,
+                          "photoURL": result.user!.photoURL
+                        });
+                      }
+                    });
+                  }),
+                  TextButton(
+                      onPressed: () {
+                        setState(() {
+                          _isLoginMode = !_isLoginMode;
+                        });
+                        _emailController.text = "";
+                        _passwordController.text = "";
+                      },
+                      child: Text(_isLoginMode ? "新規登録画面へ" : "ログイン画面へ")),
+                  ElevatedButton(
+                      onPressed: () {
+                        Navigator.of(context).pushReplacementNamed('/mainHost');
+                      },
+                      child: Text("Navigate to mainHost"))
+                ],
+              ),
             ),
-          ),
-        ],
+          ],
+        ),
       ),
     ));
   }
