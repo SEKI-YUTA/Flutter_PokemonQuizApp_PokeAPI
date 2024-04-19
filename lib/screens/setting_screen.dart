1c0e03daadce517b16cf9706403c6276f7c7794f
SEKI-YUTA <zard.since1991@gmail.com>
2024-04-19 10:05:54 +0900

    add display UserInformationCard

--- lib/screens/setting_screen.dart
+++ lib/screens/setting_screen.dart
@@ -1,9 +1,43 @@
+import 'dart:ffi';
+
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
+import 'package:pokemon_quiz_app/components/user_information_card.dart';
+import 'package:pokemon_quiz_app/data/FireStoreClient.dart';
+import 'package:pokemon_quiz_app/data/model/UserData.dart';
 
-class SettingScreen extends StatelessWidget {
+class SettingScreen extends StatefulWidget {
+  // Bool _isLoading = false;
   const SettingScreen({super.key});
+
+  @override
+  State<SettingScreen> createState() => _SettingScreenState();
+}
+
+class _SettingScreenState extends State<SettingScreen> {
+  UserData? userData;
+  bool _isLoading = false;
+
+  Future<void> _getUserData() async {
+    setState(() {
+      _isLoading = true;
+    });
+    var user = FirebaseAuth.instance.currentUser;
+    if (user != null) {
+      var userData = await FireStoreClient.getUserData(user.uid);
+      setState(() {
+        this.userData = userData;
+        _isLoading = false;
+      });
+    }
+  }
+
+  @override
+  void initState() {
+    _getUserData();
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -11,21 +45,9 @@ class SettingScreen extends StatelessWidget {
       padding: const EdgeInsets.all(16),
       child: Column(
         children: [
-          const AspectRatio(
-            aspectRatio: 1.56,
-            child: Card(
-              child: Padding(
-                padding: EdgeInsets.all(16),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Text("これまで捕まえたポケモン XX匹"),
-                    Text("登録した日 XXXX年XX月XX日")
-                  ],
-                ),
-              ),
-            ),
-          ),
+          userData != null
+              ? UserInformationCard(userData: userData!)
+              : Container(),
           Expanded(child: Container()),
           TextButton(
               onPressed: () async {
